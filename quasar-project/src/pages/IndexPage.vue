de9ee45cb28795571471b9175c5910356fa9569b
vuxi2 <roko.vukovic2@gmail.com>
2025-10-20 11:54:34 +0200

    Quasar projekt 20.10.

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="https://www.journal.hr/wp-content/uploads/2020/01/Klementinum-knji%C5%BEnica-u-Pragu-1080x1620.jpg"
+      style="width:100%; height: 100%"
+    />
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
---
+++ quasar-project/src/pages/NoveKnjigePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-ajax-bar :hijack-filter="myFilterFn" />
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+      myFilterFn (url) {
+        // example (only https://my-service.com/* should trigger)
+        return /^https:\/\/my-service\.com/.test(url)
+      }
+    }
+  }
+}
+</script>
